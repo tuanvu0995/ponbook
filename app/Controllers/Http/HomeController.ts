c55b8791e952cee1fdd12182558575ad33c773db
tuanvu0995 <tuanvu0995@gmail.com>
2023-01-19 13:33:09 +0700

    fix: sort by comment at
    Signed-off-by: tuanvu0995 <tuanvu0995@gmail.com>

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -18,13 +18,13 @@ export default class HomeController {
 
     const newlyUpdatedVideos = await Video.query()
       .innerJoin('comments', 'videos.id', 'comments.video_id')
-      .where('is_published', true)
+      .where('videos.is_published', true)
       .where('comments.is_blocked', false)
       .where('comments.is_draft', false)
       .where('videos.is_deleted', false)
-      .groupBy('videos.id')
-      .orderBy('comments.created_at', 'desc')
       .select('videos.*')
+      .orderBy('videos.updated_at', 'desc')
+      .groupBy('videos.id')
       .limit(12)
     for (const video of newlyUpdatedVideos) {
       await video.preloadImages()
