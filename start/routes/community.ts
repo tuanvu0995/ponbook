732eb978d9a6176f2ce6f043636e45b4e35ae2c3
tuanvu0995 <tuanvu0995@gmail.com>
2023-01-27 12:04:45 +0700

    feat: add auth middleware for postcomment

--- start/routes/community.ts
+++ start/routes/community.ts
@@ -4,7 +4,9 @@ Route.group(() => {
   Route.get('/', 'CommunityController.feed').as('feed')
   Route.get('/p/:uid', 'CommunityController.postDetail').as('post:detail')
 
-  Route.post('/p/:uid/comments', 'CommunityController.postComment').as('post:comment')
+  Route.post('/p/:uid/comments', 'CommunityController.postComment')
+    .as('post:comment')
+    .middleware('auth')
 })
   .as('community')
   .prefix('community')
