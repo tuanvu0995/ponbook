9225f3f6672b1aa7cc31a236aee8fee535a1567b
tuanvu0995 <tuanvu0995@gmail.com>
2023-03-28 10:45:43 +0700

    feat: change actor route
    Signed-off-by: tuanvu0995 <tuanvu0995@gmail.com>

--- app/Services/SitemapGenerator.ts
+++ app/Services/SitemapGenerator.ts
@@ -31,7 +31,7 @@ async function getCasts() {
   const casts = await Cast.query().limit(50)
   return casts.map((cast) => {
     return {
-      url: `/idols/${cast.slug}`,
+      url: `/a/${cast.slug}`,
       changefreq: 'weekly',
       priority: 0.2,
     }
