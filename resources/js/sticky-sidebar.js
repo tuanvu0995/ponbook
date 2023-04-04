250e8de462dec57393a4bc94fdc64876cbce7b10
tuanvu0995 <tuanvu0995@gmail.com>
2023-04-04 21:11:37 +0700

    fix: sidebar spacing
    Signed-off-by: tuanvu0995 <tuanvu0995@gmail.com>

--- resources/js/sticky-sidebar.js
+++ resources/js/sticky-sidebar.js
@@ -2,8 +2,8 @@ import StickySidebar from 'sticky-sidebar-v2'
 
 $(function () {
   var sidebar = new StickySidebar('#sidebar', {
-    topSpacing: 20,
-    bottomSpacing: 20,
+    topSpacing: 50,
+    bottomSpacing: 50,
     containerSelector: '#main-content',
     innerWrapperSelector: '.sidebar__inner',
     scrollContainer: 'body',
