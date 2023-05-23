21652cfe351c5d5956f6da99ecba85ebfd1f6136
tuanvu0995 <tuanvu0995@gmail.com>
2023-05-23 13:39:08 +0700

    feat: update ad banner
    Signed-off-by: tuanvu0995 <tuanvu0995@gmail.com>

--- resources/js/utils/initAds.js
+++ resources/js/utils/initAds.js
@@ -1,8 +1,8 @@
 const girlXyzUrl =
-  'https://www.liquidfire.mobi/redirect?sl=16&t=dr&track=185704_286256&siteid=286256'
+  'https://go.xlirdr.com?userId=81bf055de32f51a4ed8de64280fdd8e4a5170fd0f6150df2c7ce2da1660a0693'
 
 export default function initAds() {
-  const banner = document.getElementById('girlxyz-banner')
+  const banner = document.getElementById('stripchat-banner')
   if (banner) {
     banner.addEventListener('click', function (event) {
       event.stopPropagation()
