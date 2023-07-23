3d80ee3c88ba740c4e5def30f75f920818de6bfe
Tim Lai <tuanvu0995@gmail.com>
2023-07-23 12:04:14 +0700

    feat: cleanup
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- app/Services/SimpleWImg.ts
+++
@@ -1,8 +0,0 @@
-import sharp from 'sharp'
-import fs from 'fs'
-
-export default async function SimpleWImg(imagePath: string, newFileName: string) {
-  const data = await sharp(imagePath).webp().toBuffer()
-  fs.writeFileSync(newFileName, data, 'binary')
-  fs.unlinkSync(imagePath)
-}
