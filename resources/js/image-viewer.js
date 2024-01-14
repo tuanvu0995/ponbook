7b2c8138e65092ef3d74cfff403a5fc41da0f41f
Tim Lai <tuanvu0995@gmail.com>
2024-01-14 18:25:31 +0700

    fix: Image viewer
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- resources/js/image-viewer.js
+++ resources/js/image-viewer.js
@@ -1,27 +1,19 @@
 import ImageViewer from 'awesome-image-viewer'
 
-let imageViewer = null
-
 function onVideoImageClick(index) {
-  if (!imageViewer) {
-    const elements = document.querySelectorAll('.image-preview__item img')
-    if (!elements) return
-    const images = []
-    elements.forEach((element) => {
-      images.push({ mainUrl: element.src })
-    })
-    imageViewer = new ImageViewer({
-      images,
-      currentSelected: index,
-      isZoomable: true,
-      stretchImages: true,
-      showThumbnails: true,
-    })
-  } else {
-    console.log('onVideoImageClick', imageViewer)
-    console.log(imageViewer.show)
-    imageViewer.showImages()
-  }
+  const elements = document.querySelectorAll('.image-preview__item img')
+  if (!elements) return
+  const images = []
+  elements.forEach((element) => {
+    images.push({ mainUrl: element.src })
+  })
+  new ImageViewer({
+    images,
+    currentSelected: index,
+    isZoomable: true,
+    stretchImages: true,
+    showThumbnails: true,
+  })
 }
 
 window.onVideoImageClick = onVideoImageClick
