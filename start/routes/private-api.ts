c57435443af85472477a7252a707b05f3615bb79
Tim Lai <tuanvu0995@gmail.com>
2024-01-16 17:54:22 +0700

    feat: Health
    Signed-off-by: Tim Lai <tuanvu0995@gmail.com>

--- start/routes/private-api.ts
+++ start/routes/private-api.ts
@@ -1,6 +1,12 @@
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
 Route.group(() => {
+  Route.get('/health', async ({ response }) => {
+    const report = await HealthCheck.getReport()
+    return report.healthy ? response.ok(report) : response.badRequest(report)
+  })
+
   /*
 |--------------------------------------------------------------------------
 | Video Routes
@@ -34,8 +40,6 @@ Route.group(() => {
   })
     .as('data-sources')
     .prefix('data-sources')
-
-  // Spacer
 })
   .prefix('api')
   .as('api')
